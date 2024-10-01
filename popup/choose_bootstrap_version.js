7f0733cbaa30b19b20016c25b3563862141e3aea
Julien Déramond <juderamond@gmail.com>
2024-10-01 18:18:12 +0200

    feat: prototype to use checkboxes in the extension interface
    Co-authored-by: Christian Oliff <christianoliff@pm.me>

--- popup/choose_bootstrap_version.js
+++ popup/choose_bootstrap_version.js
@@ -1,14 +1,19 @@
-document.querySelectorAll("button").forEach((button) => {
+// TODO: handle the "reset" button
+/* document.querySelectorAll("button").forEach((button) => {
   button.addEventListener("click", switchFunction);
+}); */
+
+document.querySelectorAll('input[type="checkbox"]').forEach((checkbox) => {
+  checkbox.addEventListener("change", switchFunction);
 });
 
 function switchFunction() {
   chrome.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
-    highlightDeprecatedClasses(tabs, this.id);
+    highlightDeprecatedClasses(tabs, this.id, this.checked);
   });
 }
 
-async function highlightDeprecatedClasses(tabs, param) {
+async function highlightDeprecatedClasses(tabs, paramRef, paramChecked) {
   for (const tab of tabs) {
     try {
       await chrome.scripting.insertCSS({
@@ -24,24 +29,34 @@ async function highlightDeprecatedClasses(tabs, param) {
           tabId: tab.id,
           allFrames: true,
         },
-        func: async (param) => {
+        func: async (paramRef, paramChecked) => {
           const extensionAvailableClasses = [
             "bootstrap-browser-extension-bs-4",
             "bootstrap-browser-extension-bs-3",
           ];
 
-          document.body.classList.remove(...extensionAvailableClasses);
+          console.log(paramRef + '-' + paramChecked);
 
-          switch(param) {
+          // document.body.classList.remove(...extensionAvailableClasses);
+
+          switch(paramRef) {
             case "bs3":
-              document.body.classList.add("bootstrap-browser-extension-bs-3");
+              if (paramChecked) {
+                document.body.classList.add("bootstrap-browser-extension-bs-3");
+              } else {
+                document.body.classList.remove("bootstrap-browser-extension-bs-3");
+              }
               break;
             case "bs4":
-              document.body.classList.add("bootstrap-browser-extension-bs-4");
+              if (paramChecked) {
+                document.body.classList.add("bootstrap-browser-extension-bs-4");
+              } else {
+                document.body.classList.remove("bootstrap-browser-extension-bs-4");
+              }
               break;
           }
         },
-        args: [param],
+        args: [paramRef, paramChecked],
       });
     } catch (err) {
       console.error(`Failed to execute script: ${err}`);
